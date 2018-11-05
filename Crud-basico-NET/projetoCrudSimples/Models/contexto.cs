c309e0c894b8cda67ee65da6a6ac749089552aac
tiago <tiagoas77@gmail.com>
2018-11-04 23:41:56 -0200

    Realizei a codificação atraves de um vídeo no you tube

---
+++ Crud-basico-NET/projetoCrudSimples/Models/contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace projetoCrudSimples.Models
+{
+    public class contexto: DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
